0479a55c935177d00303288ab979e73f934507d6
mikenorgate <github@mike-norgate.com>
2024-01-11 21:47:23 +0000

    Update screenshot.png and next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  staticImage: true
+  staticImage: true,
+  images: {
+    unoptimized: true,
+  },
 })
 
 module.exports = withNextra()
